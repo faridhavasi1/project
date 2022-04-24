4611ca4efb1a81da3911c0a21a175a0b291cce6f
farid <faridhavasi@yahoo.com>
2022-04-24 20:08:28 +0430

    add pager

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,9 +1,14 @@
+from pydoc import pager
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . models import Post
 # Create your views here.
 def home(request):
    post=Post.objects.published_posts()
+   Paginator=Paginator(Post.objects.published_posts(),1)
+   page=request.GET.get('page')
+           
    context={
        'posts':post
    }
@@ -11,6 +16,7 @@ def home(request):
 
 
 def post_detail(request,slug):
+    
     post=get_object_or_404(Post,slug=slug,status='p')
     context={
         'post':post
